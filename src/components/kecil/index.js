17d2aa88cce606010ee4f1421626ef108e2c4e7d
ipan140 <132807185+ipan140@users.noreply.github.com>
2023-12-05 08:24:56 +0700

    praktikum 8
    iya

---
+++ src/components/kecil/index.js
@@ -0,0 +1,7 @@
+import Button from './button' 
+import Input from './input'
+import CategoryTab from './categoryTab'
+import Pilihan from './pilihan'
+import back_fab from './back_fab'
+
+export {Button, Input, CategoryTab, Pilihan, back_fab}
